8f658fde375a1f8f5bfbd1369367630e00e429f7
Saurabh Londhe <saurabh.londhe@emtecinc.com>
2022-05-13 20:52:33 +0530

    deployed

--- src/store/rootReducer.ts
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import flightReducer from "../reducers/flightReducer";
-
-const rootReducer = combineReducers({
-  flights: flightReducer,
-});
-
-export default rootReducer;
